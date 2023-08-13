caf2526b15b9c25d06de6c6bd6321ad3c5f5d373
lehuuhieu-0805 <01643815296h@gmail.com>
2023-08-13 17:17:27 +0700

    feat: handle error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import { errorHandler } from './middlewares/error-handler.middleware';
+import { NotFoundError } from './errors';
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4001;
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
