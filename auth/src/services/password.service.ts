73d46ca105493b13f198f7e4c4dbfe8c3b4c9aab
lehuuhieu-0805 <01643815296h@gmail.com>
2023-08-14 07:25:57 +0700

    feat: toHash password service

---
+++ auth/src/services/password.service.ts
@@ -0,0 +1,17 @@
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString('hex');
+    const buf = scrypt(password, salt, 64) as unknown as Buffer;
+
+    return `${buf.toString('hex').${salt}}`
+  }
+
+  static compare(storedPassword: string, suppliedPassword: string) {
+    
+  }
+}
