5cc0d9abb1672749463583ccac4aa05cb7bf4e46
hieulh <hieulh@cbbank.vn>
2023-08-15 17:30:14 +0700

    feat: function login

--- auth/src/routes/signin.route.ts
+++ auth/src/routes/signin.route.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { BadRequestError } from '../errors';
 import { validateRequest } from '../middlewares/validate-request.middleware';
+import { User } from '../models/user';
+import { Password } from '../services/password.service';
 
 const router = express.Router();
 
@@ -11,7 +15,33 @@ router.post(
     body('password').trim().notEmpty().withMessage('You must input a password'),
   ],
   validateRequest,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existedUser = await User.findOne({ email });
+    if (!existedUser) {
+      throw new BadRequestError('Email or password is not correct');
+    }
+
+    const passwordMatch = await Password.compare(
+      existedUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError('Email or password is not correct');
+    }
+
+    // generate JWT
+    const userJwt = jwt.sign(
+      { id: existedUser.id, email: existedUser.email },
+      process.env.JWT_KEY!
+    );
+
+    // store it on session object
+    req.session = { jwt: userJwt };
+
+    return res.status(201).json(existedUser);
+  }
 );
 
 export { router as signinRouter };
