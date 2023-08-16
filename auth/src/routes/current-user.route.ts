888c5524ebad19cfe8c5cb026dde46018a04e3e3
hieulh <hieulh@cbbank.vn>
2023-08-16 17:04:21 +0700

    feat: handle api current user with current user middleware

--- auth/src/routes/current-user.route.ts
+++ auth/src/routes/current-user.route.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
+import { currentUser } from '../middlewares';
 
 const router = express.Router();
 
-router.get('/api/users/current-user', (req: Request, res: Response) => {
-  res.status(200).json({
-    message: 'current-user',
-  });
-});
+router.get(
+  '/api/users/current-user',
+  currentUser,
+  (req: Request, res: Response) => {
+    res.json(200).json({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
