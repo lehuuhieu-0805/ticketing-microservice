a6f0d4941e61781f4400bfa432083edf64bd67de
hieulh <hieulh@cbbank.vn>
2023-08-18 17:08:06 +0700

    feat: test: sets a cookie after successful signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -51,3 +51,15 @@ it('disallows duplicate emails', async () => {
     })
     .expect(400);
 });
+
+it('sets a cookie after successful signup', async () => {
+  const respone = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  expect(respone.get('Set-Cookie')).toBeDefined();
+});
