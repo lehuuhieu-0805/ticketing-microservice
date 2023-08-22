8c05be4d02372de10ebe43c82e602d1e09ec5e2e
hieulh <hieulh@cbbank.vn>
2023-08-22 15:26:26 +0700

    feat: api create a ticket

--- tickets/src/routes/new.route.ts
+++ tickets/src/routes/new.route.ts
@@ -1,10 +1,28 @@
-import { requireAuth } from '@hieulh-ticket/common';
+import { requireAuth, validateRequest } from '@hieulh-ticket/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { Ticket } from '../models/ticket.model';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').trim().notEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
+    await ticket.save();
+
+    res.status(200).json(ticket);
+  }
+);
 
 export { router as createTicketRouter };
