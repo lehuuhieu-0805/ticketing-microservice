65e3cbb23f191b7a2f3195741cf00b443e4d7faa
hieulh <hieulh@cbbank.vn>
2023-08-22 17:29:04 +0700

    feat: api update tickets

---
+++ tickets/src/routes/update.route.ts
@@ -0,0 +1,42 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@hieulh-ticket/common';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { Ticket } from '../models/ticket.model';
+
+const router = express.Router();
+
+router.put(
+  '/api/tickets/:id',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be provided and must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    ticket.set({ title: req.body.title, price: req.body.price });
+
+    await ticket.save();
+
+    res.status(200).json(ticket);
+  }
+);
+
+export { router as updateTicketRouter };
